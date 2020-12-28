2fdae8f0e1ea2a4b5256078aa539ebb7f5dc1b32
Robin Abrahamsson <robin@abrahamsson.tech>
2020-12-28 16:48:59 +0100

    Added F# repl

---
+++ impls/fsharp2/README.md
@@ -0,0 +1,2 @@
+
+1. Run repl: `dotnet run -p step0_repl`
---
+++ impls/fsharp2/step0_repl/Program.fs
@@ -0,0 +1,29 @@
+﻿module REPL
+  open System
+
+  let READ input =
+    input
+
+  let EVAL data =
+    data
+
+  let PRINT input =
+    printfn "%s" input
+
+  let REP input =
+    input
+    |> READ
+    |> EVAL
+    |> PRINT
+
+  let readLine () =
+    printf "user> "
+    Console.ReadLine()
+
+  [<EntryPoint>]
+  let rec main _argv =
+      match readLine () with
+      | null -> 0
+      | input ->
+        REP input
+        main _argv
